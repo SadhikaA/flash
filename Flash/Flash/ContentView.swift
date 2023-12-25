14b5753aff51ad91c67c3d30d69d3edd1a1a7332
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-12-24 19:28:58 -0800

    starting flashcard app w/ swift data

---
+++ Flash/Flash/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Flash
+//
+//  Created by Sadhika Akula on 12/24/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Text("hello world")
+        }
+    }
+
+   
+}
+
+#Preview {
+    ContentView().navigationTitle("Flash")
+}
---
+++ Flash/Flash/FlashApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlashApp.swift
+//  Flash
+//
+//  Created by Sadhika Akula on 12/24/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlashApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }    }
+}
